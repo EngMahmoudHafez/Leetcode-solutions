20c593bb97186047027e3e89be7648702abfc794
Mahmoud Hafez <97672919+EngMahmoudHafez@users.noreply.github.com>
2024-05-16 08:01:12 +0300

    Time: 12 ms (47.07%) | Memory: 17.5 MB (24.21%) - LeetSync

---
+++ 2416-evaluate-boolean-binary-tree/evaluate-boolean-binary-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool solve(TreeNode *root)
+    {
+        if(root->left==NULL && root->right==NULL)
+            return root->val;
+        bool left=solve(root->left);
+        bool right=solve(root->right);
+        if(root->val==2)
+            return left | right;
+        else
+            return left & right;
+    }
+    bool evaluateTree(TreeNode* root) {
+        return solve(root);
+    }
+};
